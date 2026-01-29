a5a341346d441ed5bf4426194f5e5b56a9d7f5cf
ArjunR27 <epicarjun27@gmail.com>
2026-01-29 13:12:02 -0800

    fixing cron job

--- backend/run_cron.py
+++ backend/run_cron.py
@@ -3,16 +3,9 @@ from datetime import datetime
 from album_tracking import track_all_users_recently_listened
 
 print("Cron job starting...")
-while True:
-    print(f"Running task at {datetime.now()}")
-    try:
-        track_all_users_recently_listened()
-        print("Task completed successfully")
-    except Exception as e:
-        print(f"Error running task: {e}")
-        
-        # Sleep for 60 seconds to avoid running multiple times in the same minute
-        time.sleep(60)
-    
-    # Check every 30 seconds
-    time.sleep(30)
+print(f"Running task at {datetime.now()}")
+try:
+    track_all_users_recently_listened()
+    print("Task completed successfully")
+except Exception as e:
+    print(f"Error running task: {e}")
