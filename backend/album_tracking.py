e54b02806ddedd1cf9ced2a62749ca9c111ec6e5
ArjunR27 <epicarjun27@gmail.com>
2026-02-11 17:09:45 -0800

    remove the celery stuff

--- backend/album_tracking.py
+++ backend/album_tracking.py
@@ -5,29 +5,10 @@ from env import load_environment
 from supabase import create_client, Client
 import os
 import time
-from celery import Celery, group
-from celery.schedules import crontab
 from validate_token import get_valid_token
 
 load_environment()
 supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_API_KEY"))
-celery = Celery("album_tracking", broker=os.getenv("REDIS_URL", "redis://localhost:6379/0"))
-celery.conf.timezone = 'UTC'
-
-
-# this is added to the redis queue
-# when the time is up the queue is the task is popped from redis queue
-# then celery uses the scheduler to determine if the task should be run
-    ## beat schedules the task on the queue every 0th minute of each hour --> tells the worker to run
-    ## worker = runs the task below. 
-
-celery.conf.beat_schedule = {
-    'track-listening-every-hour': {
-        'task': 'album_tracking.track_all_users_recently_listened',
-        'schedule': crontab(minute=45)
-        # 'schedule': crontab(minute='*/5'),  # Every 5 minutes
-    },
-}
 
 def _get_playthrough_user_id(spotify_user_id):
     resp = (
